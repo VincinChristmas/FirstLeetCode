8a67db2d428fb7d9714b8b96f33b964a7f52f0de
VincinChristmas <VincinChristmas@hotmail.com>
2024-06-10 14:44:31 +0800

    redid a leetcode problem today first solving it with pseudocode

--- main.js
+++ main.js
@@ -1,3 +1,49 @@
+// DID RUNNING SUM ARRAY AGAIN FOR REPITITION AND PRACTICE WITH A SLIGHTLY DIFFERENT SOLUTION AND PSEUDOCODE
+
+//1480. Running Sum of 1d Array
+
+/*
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: [1,2,3,4,5]
+Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+Example 3:
+
+Input: nums = [3,1,2,10,1]
+Output: [3,4,6,16,17]
+ 
+
+Constraints:
+
+1 <= nums.length <= 1000
+-10^6 <= nums[i] <= 10^6
+*/
+
+//MY SOLUTION
+var runningSum = function(nums) {
+    //iterate through the array
+    //array the previous array to current one and show the sum
+    //return entire array when done
+    for (let i = 1; i < nums.length; i++) {
+        nums[i] = nums[i - 1] + nums[i]
+    }
+    return nums
+};
+
+
+
 //2665. Counter II
 
 /*
