11cdeabc9850367bb5fdf6bb394f50b6a0518e96
VincinChristmas <VincinChristmas@hotmail.com>
2024-06-10 16:49:59 +0800

    solved a fizzbuzz array problem for leetcode

--- main.js
+++ main.js
@@ -1,3 +1,54 @@
+//412. Fizz Buzz
+
+/*
+Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["1","2","Fizz"]
+Example 2:
+
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
+Example 3:
+
+Input: n = 15
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ 
+
+Constraints:
+
+1 <= n <= 104
+*/
+
+//MY SOLUTION
+var fizzBuzz = function(n) {
+    let arrGame = []
+    let sum = 0
+    while (sum < n) {
+        sum = sum + 1
+        if (sum % 3 === 0 && sum % 5 === 0) {
+            arrGame.push("FizzBuzz")
+        }
+        else if (sum % 3 === 0) {
+            arrGame.push("Fizz")
+        } else if (sum % 5 === 0) {
+            arrGame.push("Buzz")
+        } else {
+          arrGame.push(String(sum))
+        }
+    
+    }
+    return arrGame
+};
+
 //1672. Richest Customer Wealth
 
 /*
